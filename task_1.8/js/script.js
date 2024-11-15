d5e331628b0474a4f52dd921aa4c5c50145dc324
U-IMC\o.paziura <o.paziura@o-paziura.imc.loc>
2024-11-15 17:34:06 +0200

    Replace js stylisation with css classes

--- task_1.8/js/script.js
+++ task_1.8/js/script.js
@@ -1,53 +1,20 @@
 let nav = document.getElementsByClassName('nav')[0];
-let initialNav = window.getComputedStyle(nav);
-
 let navMenu = document.getElementsByClassName('nav_menu')[0];
-let navItems = document.getElementsByClassName('nav_item');
-
 let menuOpener = document.getElementsByClassName('menu_opener')[0];
-let menuBlur = document.getElementsByClassName('burger_menu-blur')[0];
 let isOpen = false;
 
 menuOpener.addEventListener('click', function () {
     if (!isOpen) {
-        nav.style.height = '290px';
         menuOpener.style.transform = 'rotate(180deg)';
 
-        Object.assign(navMenu.style, {
-            position: 'absolute',
-            display: 'flex',
-            opacity: '1',
-            flexDirection: 'column',
-            gap: '5px',
-            top: '100px',
-            left: '0px',
-            width: '100%',
-            textAlign: 'center'
-        })
-
-        Object.assign(menuBlur.style, {
-            top: '290px',
-            opacity: 0.8,
-            visibility: 'visible'
-        })
-        
-        Array.from(navItems).forEach(navItem => {
-            Object.assign(navItem.style, {
-                height: '60px',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center'
-            })
-        })
+        navMenu.classList.add('sliding-nav_menu');
+        nav.classList.add('sliding-nav');
     }
     else {
-        nav.style = initialNav;
         menuOpener.style.transform = 'rotate(0)';
-        navMenu.style.display = 'none';
-        Object.assign(menuBlur.style, {
-            visibility: 'hidden',
-            opacity: 0
-        })
+
+        navMenu.classList.remove('sliding-nav_menu');
+        nav.classList.remove('sliding-nav');
     }
 
     isOpen = !isOpen;
